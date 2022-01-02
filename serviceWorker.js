801ce3c1991102619a16500f3dd482f82d681280
Abd Alrazzaq <devoojs@gmail.com>
2022-01-02 15:47:49 -0800

    gitxxx

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const pawxmr = "paw-xmr-v1";
 const assets = [
   "/",
   "/index.html",
@@ -8,7 +8,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(pawxmr).then(cache => {
       cache.addAll(assets);
     })
   );
